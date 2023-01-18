1fd34087ec07842e58ba638fcf93a4c286eaa6d3
Taehun Lee <thiskorea@cberi.go.kr>
2023-01-18 14:41:48 +0900

    end

--- sketch_jan18a_analog/sketch_jan18a_analog.ino
+++ sketch_jan18a_analog/sketch_jan18a_analog.ino
@@ -7,15 +7,15 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  // read the input on analog pin 0:
+  //** put your main code here, to run repeatedly:
+  //** read the input on analog pin 0:
   int sensorValue = analogRead(A0);
-  // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
-  float voltage = sensorValue * (5.0 / 1023.0);
-  // print out the value you read:
-  Serial.println(voltage);
-
+  //** Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
+  // float voltage = sensorValue * (5.0 / 1023.0);
+  //** print out the value you read:
+  // Serial.println(voltage);
+  //** Convert to the digital value(0 - 255)
   int digitalValue = (int)sensorValue * (255.0 / 1023.0);
   Serial.println(digitalValue);
-  analogWrite(red, digitalValue);
+  analogWrite(red, digitalValue);  
 }
