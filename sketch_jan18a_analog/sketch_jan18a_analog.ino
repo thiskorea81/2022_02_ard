1a7a9331e590515a0fe7a9b17d4d92ddcebee185
Taehun Lee <thiskorea@cberi.go.kr>
2023-01-20 11:31:42 +0900

    error corrected

--- sketch_jan18a_analog/sketch_jan18a_analog.ino
+++ sketch_jan18a_analog/sketch_jan18a_analog.ino
@@ -1,5 +1,6 @@
 int red=11;
-bool flag=true;
+bool flag=false;
+int touchN=0;
 
 void setup() {
   // put your setup code here, to run once:
@@ -7,40 +8,24 @@ void setup() {
   pinMode(red, OUTPUT);
   
 }
-int touchN=0;
-
 
 void loop() {
-  //** put your main code here, to run repeatedly:
-  //** read the input on analog pin 0:
   int sensorValue = analogRead(A0);
-  //** Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
-  // float voltage = sensorValue * (5.0 / 1023.0);
-  //** print out the value you read:
-  // Serial.println(voltage);
-  //** Convert to the digital value(0 - 255)
   int digitalValue = (int)sensorValue * (255.0 / 1023.0);
-  //Serial.println(sensorValue);
-  Serial.println(flag);
-  if (!flag && sensorValue>995){
-    //Serial.print("touched ");
-    //Serial.println(flag);
-    //Serial.println(sensorValue);
-    
+  
+
+  if (sensorValue>995){    
     digitalWrite(red, HIGH);
-    touchN++;
+    if(!flag)
+      touchN++;
     Serial.print(touchN);
     Serial.print(", ");
     Serial.println(sensorValue);    
     
     flag=true;  
   }
-  else{
-    //Serial.print("untouched ");
-    //Serial.println(flag);
-    //Serial.println(sensorValue);
-    
-    //Serial.println("bright");
+  else {    
+    Serial.println(flag);
     digitalWrite(red, LOW);
     flag=false;
   }
--- sketch_jan20a/sketch_jan20a.ino
+++ sketch_jan20a/sketch_jan20a.ino
@@ -1,9 +1,25 @@
+int red=11;
+bool eventT=false;
+int touchN=0;
+int aa[]={1,1,1,0,0,1,0,1};
+int n=0;
 void setup() {
   // put your setup code here, to run once:
-
+  Serial.begin(9600);
+  pinMode(red, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  Serial.print(eventT);
+  Serial.print(" : ");
+  if(eventT && n<8){
+    Serial.println(aa[n]);
+    n++;
+  }
+  else{
+    Serial.println();
+  }
+  delay(1000);
+  eventT=!eventT;
 }
